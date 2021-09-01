96a92867887b9ede9e1ec0a2048e354556871e7b
Christophe Diederichs <chris.d.itunes@gmail.com>
2021-09-01 18:30:17 +0100

    use uint based encoding for bitfield

--- bitfield.js
+++ bitfield.js
@@ -1,29 +1,32 @@
 const c = require('compact-encoding')
 
 module.exports = {
-  preencode (state, bits = []) {
-    const n = Math.ceil(bits.length / 8)
-    const len = (n <= 0xfc ? 1 : n <= 0xffff ? 3 : n <= 0xffffffff ? 5 : 9) + n
-    state.end += len
+  preencode (state, bits = [], prev) {
+    c.uint.preencode(state, bitsToNumberLE(bits))
   },
   encode (state, bits = []) {
-    const bitfield = Buffer.alloc(Math.ceil(bits.length / 8))
-    for (let i = 0; i < bits.length; i++) {
-      const index = i >>> 3
-      bitfield[index] |= bits[i] << i % 8
-    }
-    c.buffer.encode(state, bitfield)
+    const num = bitsToNumberLE(bits)
+    c.uint.encode(state, num)
   },
   decode (state) {
-    const bits = []
-    const buf = c.buffer.decode(state)
-    if (!buf) return bits
-
-    const bitLen = buf.byteLength << 3
-    for (let i = 0; i < bitLen; i++) {
-      const index = i >>> 3
-      bits[i] = (buf[index] & (1 << i % 8)) !== 0
-    }
-    return bits
+    const num = c.uint.decode(state)
+    return numberToBitsLE(num)
   }
+}
+
+function bitsToNumberLE (bits) {
+  let num = 0
+  for (let i = 0; i < bits.length; i++) {
+    num |= bits[i] << i
+  }
+  return num
+}
+
+function numberToBitsLE (num) {
+  const bits = []
+  while (num > 0) {
+    bits.push(num & 1)
+    num >>>= 1
+  }
+  return bits
 }
